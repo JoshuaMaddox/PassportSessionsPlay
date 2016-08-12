7046d9fa4e2140084ad146d8f523bf913adb5fcc
Joshua <joshua@eduku.org>
2016-08-12 10:24:05 +0000

    added the secret route that will later be used to test Auth

--- authentication/authDemo/app.js
+++ authentication/authDemo/app.js
@@ -13,6 +13,10 @@ app.get("/", function(req, res){
    res.render("home"); 
 });
 
+app.get("/secret", function(req, res){
+    res.render("secret");
+})
+
 //Start Server
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Auth Demo Server Has SpunUp");
