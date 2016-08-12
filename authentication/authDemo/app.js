f972e10d4d7c00ba4fae007722cd35ec9359d40b
Joshua <joshua@eduku.org>
2016-08-12 10:19:31 +0000

    added the index route and the start server command

--- authentication/authDemo/app.js
+++ authentication/authDemo/app.js
@@ -3,3 +3,17 @@ var app = express();
 
 //Setting up ejs
 app.set('view engine', 'ejs');
+
+/*=========================================
+    |    |    ROUTES
+=========================================*/
+
+
+app.get("/", function(req, res){
+   res.render("home"); 
+});
+
+//Start Server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Auth Demo Server Has SpunUp");
+});
